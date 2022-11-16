74a0f7a9f58a188d41c31a681ffaa065be782261
Taiki Sonoyama <109128636+fullpowerz1@users.noreply.github.com>
2022-11-16 12:47:29 +0000

    zan

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,8 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+    has_many :cart_items, class_name: "Customer", foreign_key: "customer_id", dependent: :destroy
+
+
 end
