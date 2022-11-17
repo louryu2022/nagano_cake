c8005ca0e026610ac8c86afcc0a6232b0715bd39
Taiki Sonoyama <109128636+fullpowerz1@users.noreply.github.com>
2022-11-17 05:06:01 +0000

    man

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,7 +4,7 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-    has_many :cart_items, class_name: "Customer", foreign_key: "customer_id", dependent: :destroy
-
-
+    has_many :cart_items, dependent: :destroy
+    has_many :orders, dependent: :destroy
+    has_many :deliveries, dependent: :destroy
 end
