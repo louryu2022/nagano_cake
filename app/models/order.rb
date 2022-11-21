b472321738c84b1b1ecb74a494ed54d48ff13680
j-shigeoka <114391855+j-shigeoka@users.noreply.github.com>
2022-11-21 11:02:04 +0000

    [Add]order

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,14 @@ class Order < ApplicationRecord
    has_many :order_details, dependent: :destroy
   
    belongs_to :customer
+   
+  def set_date
+    created_at.strftime("%Y/%m/%d %H:%M:%S")
+  end
+  
+  enum status: { waiting: 0, confirmation: 1, production: 2, preparation: 3, sent: 4}
+  
+  def full_name
+    self.first_name + self.last_name
+  end
 end
