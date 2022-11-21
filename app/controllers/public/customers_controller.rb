012a8bb99c797aea28260ca4f27a40ac030b208f
ryoga <115019652+ryoga0703@users.noreply.github.com>
2022-11-21 11:02:25 +0000

    会員退会　ほぼ完成

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,17 +1,30 @@
 class Public::CustomersController < ApplicationController
   def show
+    @customer = current_customer
   end
 
   def edit
+    @customer = current_customer
   end
 
   def update
+    @customer = current_customer
+    if @customer.update(customer_params)
+      redirect_to customers_my_page_path
+    else
+      render :customers_infomation_edit_path
+    end
   end
 
   def unsubscribe
+    @customer = current_customer
   end
 
   def withdraw
+    @customer = current_customer
+    @customer.update(is_deleted: true)
+    reset_session
+    redirect_to root_path
   end
 
   private
--- db/migrate/20221120063828_remove_adress_from_customers.rb
+++
@@ -1,5 +0,0 @@
-class RemoveAdressFromCustomers < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :customers, :adress, :string
-  end
-end
